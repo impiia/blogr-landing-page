1e1d372687193c171aa733e953ea6892161cde27
impiia <impiia28@gmail.com>
2024-04-19 23:38:27 +0400

    fix arrow acins styles

--- src/components/menu-item/component.jsx
+++ src/components/menu-item/component.jsx
@@ -1,60 +1,74 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useCallback } from 'react';
 import ArrowIcon from '../../assets/icon-arrow-light.svg';
 import styles from './styles.module.scss';
 import { createPortal } from 'react-dom';
 import { Dropdown } from '../dropdown/component';
 import classNames from 'classnames';
 
+const MOBILE_WIDTH = 770;
+
 export const MenuItem = ({ title, subItems }) => {
-  const menuItemRef = useRef(); 
-  const [isOpen, setIsOpen] = useState(false); 
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 770); 
+  const [coordinates, setCoordinates] = useState(null);
+  const menuItemRef = useRef();
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= MOBILE_WIDTH);
+  const [isOpen, setIsOpen] = useState(false);
+  const popoverContainer = useRef(null);
 
-  const handleClick = useCallback(() => {
-    setIsOpen(prevIsOpen => !prevIsOpen);
+  const toggleMenu = useCallback(() => {
+    setIsOpen(prev => !prev);
   }, []);
 
-  const handleResize = useCallback(() => {
-    setIsMobile(window.innerWidth <= 770);
-  }, []);
-
-  const handleClickOutside = useCallback((event) => {
-    if (isOpen && !menuItemRef.current.contains(event.target)) {
-      setIsOpen(false);
+  const toggleModal = useCallback(() => {
+    if (coordinates) {
+      setCoordinates(null);
+      return;
     }
-  }, [isOpen]);
+    const { bottom, left } = menuItemRef.current.getBoundingClientRect();
+    setCoordinates({ left, top: bottom });
+  }, [coordinates]);
 
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= MOBILE_WIDTH);
+      if (coordinates) {
+        const { bottom, left } = menuItemRef.current.getBoundingClientRect();
+        setCoordinates({ left, top: bottom });
+      }
+    };
+
+    const handleClickOutside = (event) => {
+      if (popoverContainer.current && !menuItemRef.current.contains(event.target) &&
+          !popoverContainer.current.contains(event.target)) {
+        setCoordinates(null);
+      }
+    };
+
+    const handleScroll = () => {
+      setCoordinates(null);
+    };
+
+    window.addEventListener("resize", handleResize);
     document.addEventListener('mousedown', handleClickOutside);
-    
+    window.addEventListener('scroll', handleScroll);
+
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
       document.removeEventListener('mousedown', handleClickOutside);
+      window.removeEventListener('scroll', handleScroll);
     };
-  }, [handleResize, handleClickOutside]);
+  }, [coordinates]);
 
   useEffect(() => {
-    setIsMobile(window.innerWidth <= 770);
-    function handleResize() {
-      setIsMobile(window.innerWidth <= 770);
-    }
-
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
+    popoverContainer.current = document.getElementById("popover-container");
   }, []);
 
-  // Вычисление координат при открытии подменю
-  const getCoordinates = useCallback(() => {
-    if (menuItemRef.current && !isMobile) {
-      const { bottom, left } = menuItemRef.current.getBoundingClientRect();
-      return { left, top: bottom };
+  const handleClick = () => {
+    if (isMobile) {
+      toggleMenu();
+    } else {
+      toggleModal();
     }
-    // Обеспечиваем позиционирование в мобильной версии или когда menuItemRef.current не доступен
-    return { left: 0, top: 0 };
-  }, [isMobile]);
+  };
 
   return (
     <div ref={menuItemRef} className={styles.root} onClick={handleClick}>
@@ -62,20 +76,18 @@ export const MenuItem = ({ title, subItems }) => {
       <img
         src={ArrowIcon}
         alt="arrow"
-        className={classNames(styles.icon, { [styles.up]: isOpen })}
+        className={classNames(styles.icon, { [styles.up]: coordinates || isOpen })}
       />
-      {isMobile && isOpen && (
+      {isMobile ? (isOpen && (
         <div className={styles.dropdown_menu}>
           <Dropdown items={subItems} />
         </div>
-      )}
-
-      {!isMobile && isOpen && createPortal(
-        <div style={getCoordinates()} className={styles.popoverContainer}>
+      )) : (coordinates && createPortal(
+        <div style={coordinates} ref={popoverContainer} className={styles.popoverContainer}>
           <Dropdown items={subItems} />
         </div>,
-        document.body // Используем document.body для портала
-      )}
+        document.body
+      ))}
     </div>
   );
 };
